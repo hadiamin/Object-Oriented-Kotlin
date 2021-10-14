a022d2d8a66dbdba1cc03545f9d2a3c1f4bab32f
Amin <aminhadi65@gmail.com>
2021-10-14 17:03:00 +0000

    SimpleAgent class implementation completed

--- src/main/kotlin/HuntAction.kt
+++ src/main/kotlin/HuntAction.kt
@@ -1,2 +1,2 @@
-class HuntAction(animal : String) {
+class HuntAction(animal : String) : Action() {
 }
--- src/main/kotlin/SimpleAgent.kt
+++ src/main/kotlin/SimpleAgent.kt
@@ -1,9 +1,14 @@
-class SimpleAgent(agentName : String) : Actor() {
+class SimpleAgent(override val name: String) : Actor {
+
     override fun perceive(vararg facts: Percept) {
         TODO("Not yet implemented")
     }
 
-    override fun act(): Action {
-        TODO("Not yet implemented")
+    override fun act() : Action {
+        return ForageAction()
+    }
+
+    override fun toString(): String {
+        return "This is a simple agent"
     }
 }
